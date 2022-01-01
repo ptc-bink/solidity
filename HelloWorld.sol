3f58c4e6aa191d294ed91a6a41ce599766d90c6b
sakele1026 <sakelejames@gmail.com>
2022-01-01 22:43:59 +0530

    Init HelloContracts

---
+++ HelloWorld.sol
@@ -0,0 +1,8 @@
+//"SPDX-Licence-Identifier: <SPDX-Licence>"
+pragma solidity ^0.8.11;
+
+contract HelloWorld {
+    function get()public pure returns (string memory){
+        return 'Hello Contracts';
+    }
+}
